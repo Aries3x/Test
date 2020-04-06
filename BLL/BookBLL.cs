aa3e6e87d0c87da75ccc5b6a5dfd01d830ea8ec4
PeiHaH <pyf0329@qq.com>
2020-04-06 20:22:42 +0800

    测试

---
+++ BLL/BookBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Entity;
+
+namespace BLL {
+    public class BookBLL {
+        BookEntity book = new BookEntity();
+
+        public void Create();
+
+        private void create();
+    }
+}
---
+++ Entity/BookEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SqlSugar;
+
+namespace Entity {
+    /// <summary>
+    /// 图书 实体类
+    /// </summary>
+    [SugarTable("T_Book")]
+    public class BookEntity {
+        /// <summary>
+        /// 图书编号
+        /// </summary>
+        public int BookId { get; set; }
+
+        /// <summary>
+        /// 图书名称
+        /// </summary>
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ModifyDate { get; set; }
+
+        public int AdminId { get; set; }
+    }
+}
